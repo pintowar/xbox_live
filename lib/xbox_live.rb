7951102c53bdbfe17adc9cb2d29f98eb5b8a728a
Mike Fischer <mikefischer99@gmail.com>
2011-11-06 14:10:58 -0500

    Minor clean-up.

--- lib/xbox_live.rb
+++ lib/xbox_live.rb
@@ -1,24 +1,21 @@
-require "mechanize"
-require "xbox_live/version"
-require "xbox_live/scraper"
-require "xbox_live/profile_page"
-require "xbox_live/games_page"
-require "xbox_live/achievements_page"
+require 'mechanize'
+require 'xbox_live/version'
+require 'xbox_live/scraper'
+require 'xbox_live/game_info'
+require 'xbox_live/profile_page'
+require 'xbox_live/games_page'
+require 'xbox_live/achievements_page'
 
 module XboxLive
 
-  class << self
-
-    # Provides configurability.
-    def options
-      @options ||= {
-        :username => nil,
-        :password => nil,
-        :refresh_age => 60,  # data will be re-fetched if older than X seconds
-        :url_prefix => 'http://live.xbox.com'
-      }
-    end
-
+  # Provides configurability.
+  def self.options
+    @options ||= {
+      :username => nil,
+      :password => nil,
+      :refresh_age => 60,  # data will be re-fetched if older than X seconds
+      :url_prefix => 'http://live.xbox.com'
+    }
   end
 
 end
