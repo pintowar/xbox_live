b22ccb5fdd15643d1aabfb9a3234287c2e99d6a7
Mike Fischer <mikefischer99@gmail.com>
2011-11-06 19:29:07 -0500

    More verbose comments and some minor clean-up.

--- lib/xbox_live/scraper.rb
+++ lib/xbox_live/scraper.rb
@@ -9,40 +9,61 @@ module XboxLive
     # Load a page from Xbox Live and return a Mechanize/Nokogiri page
     # TODO: cache pages for some time to prevent duplicative HTTP activity
     def self.get_page(url)
+      # Load the specified page via Mechanize
       log "Getting page #{url}"
-
       page = agent.get(url)
+
+      # Most pages require authentication. If the Mechanize agent has
+      # not logged in yet, or if the session has expired, it will be
+      # redirected to the Xbox Live login page.
       if login_page?(page)
+        # Log the agent in via the returned login page.
         log "Page load failed - not signed in."
         page = login(page)
+
+        # The login SHOULD have returned the original page requested,
+        # but the URL will be the POST URL, so there is no way to be
+        # certain. Therefore, it is safest to just load the page again
+        # now that the Mechanize agent has logged in.
+        log "Retrying page #{url}"
         page = agent.get(url)
       end
 
-      if page.nil? or page.uri.to_s != url
-        log "FATAL: loaded page URL does not match expected URL. Loaded: #{page.uri.to_s}"
+      if page.nil?
+        log "ERROR: failed to load page."
+        return nil
+      end
+
+      if page.uri.to_s != url
+        log "ERROR: loaded page URL does not match expected URL. Loaded: #{page.uri.to_s}"
         return nil
       end
 
       log "Loaded page #{page.title}"
-      return page
+      page
     end
+
 
     private
 
-    # Log in to Xbox Live using the supplied login page
+    # Log in to Xbox Live using the supplied login page.
     def self.login(page)
       return nil if !login_page?(page)
 
-      # No science, just voodoo. Figured all this out through trial and
-      # error.
+      # Find the URL where the login form should be POSTed to.
       url = page.body.match(/srf_uPost='([^']+)/)[1]
-      ppft_html = page.body.match(/srf_sFT='([^']+)/)[1]
-      ppft = ppft_html.match(/value="([^"]+)/)[1]
       if url.empty?
-        log "FATAL: Trying to log in but Sign In page doesn't contain needed info."
+        log "ERROR: Trying to log in but 'Sign In' page doesn't contain needed info."
         return nil
       end
 
+      # PPFT appears to be some kind of session identifier which is
+      # required for the login process.
+      ppft_html = page.body.match(/srf_sFT='([^']+)/)[1]
+      ppft = ppft_html.match(/value="([^"]+)/)[1]
+
+      # The rest of the parameters are either user-provided (i.e.
+      # username and password) or are constants.
       params = {
         'login' => XboxLive.options[:username],
         'passwd' => XboxLive.options[:password],
@@ -53,22 +74,27 @@ module XboxLive
         'PPFT' => ppft,
         'idshbo' => '1'
       }
+
+      # POST the login form and hope for the best.
+      log "Submitting login form via POST"
       page = agent.post(url, params)
 
       # The login will fail and return a page saying that Javascript must be
-      # enabled, but there's a hidden form in the page that can be submitted to
-      # enable non-javascript support.
+      # enabled. However, there is a hidden form in the page that can be
+      # submitted to enable non-javascript support.
       form = page.form('fmHF')
       if form.nil?
-        log "FATAL: Trying to log in but non-JS page doesn't contain form fmHF."
+        log "ERROR: The non-JS login page doesn't contain form fmHF."
         return nil
       end
 
-      page = agent.submit(form)
-      return page
+      # Submitting the form on the Javascript error page completes the
+      # login process, and SHOULD return the originally requested page.
+      log "Submitting final non-JS login form"
+      agent.submit(form)
     end
 
-    # Is the given page the Xbox Live login page?
+    # Check to see if the provided page the Xbox Live login page.
     def self.login_page?(page)
       page and page.title == "Welcome to Windows Live"
     end
